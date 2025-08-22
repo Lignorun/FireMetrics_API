44af1bde967e0d25f973df14f8c136b3b190daf3
Diego Ferreira <dlins_ferreira@hotmail.com>
2025-08-22 10:58:59 -0400

    starting the basic statistics calc

---
+++ math/basic_data.py
@@ -0,0 +1,8 @@
+# math.basic_data.py
+
+from data.cache_manager import (
+    get_raw_data_from_cache,
+    set_raw_data_to_cache)
+
+def basic_stats(local_type: str, local_code: str, grouping: str, mode: str):
+    return None
